abbeefe822cb86863a101708f94617915cf0c670
SoumyataJena <soumyatajena@gmail.com>
2025-02-18 21:35:35 -0500

    Added more string manipulation code

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ class CodingPractice
 {
     internal static void StringManipulation(string input)
     {
+        string newInput = input;
+
         // 1. Reverse the string
         // string are immutatble , hence always convert them to char[] first to do any modification on it as char[] is mutatable
         char[] originalStr1 = input.ToCharArray();
@@ -68,18 +70,71 @@ class CodingPractice
         Console.WriteLine();
 
         // 6. Find all possible substring of a given string
-        // abc a b c ab bc abc
-        Console.WriteLine($"Find all possible substrings in string {input} =>");
-        for (int i = 0; i < input.Length; i++)
+        // abc => a b c ab bc abc
+        Console.WriteLine($"Find all possible substrings in string {newInput} =>");
+        for (int i = 0; i < newInput.Length; i++)
         {
-            var subStr = new StringBuilder(input.Length - i);
-            for (int j = i; j < input.Length; j++)
+            for (int j = i; j < newInput.Length; j++)
             {
-                subStr.Append(input[j]);
-                Console.Write(subStr + " ");
+                Console.Write(newInput.Substring(i, j - i + 1) + " ");
             }
         }
+        //for (int i = 0; i < input.Length; i++)
+        //{
+        //    var subStr = new StringBuilder(input.Length - i);
+        //    for (int j = i; j < input.Length; j++)
+        //    {
+        //        subStr.Append(input[j]);
+        //        Console.Write(subStr + " ");
+        //    }
+        //}
         Console.WriteLine();
+
+        // 7, Find all possible subsets of a given string
+        // abc => "" a b ab c ac bc abc
+        Console.WriteLine($"Find all possible subsets in string {input} =>");
+        List<string> subsets = new List<string>();
+        subsets.Add("");
+        for (int i=0;i<newInput.Length;i++)
+        {
+            int count = subsets.Count;
+            for(int j=0;j<count; j++)
+            {
+                subsets.Add(subsets[j] + newInput[i]);
+            }
+        }
+        foreach (var sub in subsets)
+        {
+            Console.Write($"{{{sub}}}");
+        }
+        Console.WriteLine();
+
+        // 8.  Perform Left circular rotation of an array by 1 place
+        // 1 2 3 4 5 => 2 3 4 5 1
+        Console.Write($"Left circular rotation of '{newInput}' by 1 place =>");
+        originalStr2 = newInput.ToCharArray();
+        char temp = originalStr2[0]; // first element
+        for(int i=1;i<= originalStr2.Length - 1; i++)
+        {
+            originalStr2[i - 1] = originalStr2[i];
+        }
+        originalStr2[^1] = temp;
+        Console.Write(string.Join("",originalStr2));
+        Console.WriteLine();
+
+        // 9. Perform Right circular rotation of an array by 1 place
+        // 1 2 3 4 5 => 5 1 2 3 4
+        Console.Write($"Right circular rotation of '{newInput}' by 1 place =>");
+        originalStr2 = newInput.ToCharArray();
+        temp = originalStr2[^1]; // last element
+        for(int i=1;i<originalStr2.Length-1;i++)
+        {
+            originalStr2[i]=originalStr2[i-1];
+        }
+        originalStr2[0] = temp;
+        Console.Write(string.Join("", originalStr2));
+        Console.WriteLine();
+
     }
     internal static bool PalindromeCheck(string input)
     {
@@ -124,10 +179,43 @@ class CodingPractice
         }
 
     }
+    internal static void PrimeCheck(int input)
+    {
+        // Prime Check
+        bool prime = false;
+        if (input == 0 || input == 1) prime = false;
+        else if (input == 2) prime = true;
+        else
+        {
+            for (int i = 2; i < input / 2; i++)
+            {
+                if (input % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+                else prime = true;
+            }
+        }
+        if(prime) Console.WriteLine($"{input} is a prime");
+        else Console.WriteLine($"{input} is not a prime");
+
+        // Find the sum of digits of the positive integer
+        int sum = 0;
+        if(input.ToString().Length == 1)
+        {
+            Console.WriteLine($"Sum of digits of {input} is {input}");
+            return;
+        }
+        int[] arr = [.. input.ToString().ToCharArray().Select(x => int.Parse(x.ToString()))];
+        foreach (var i in arr)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"Sum of digits of {input} is {sum}");
+    }
     internal static void ArrManipulation(Array input)
     {
-
-
     }
 
 }
@@ -141,7 +229,7 @@ class Program()
         do
         {
             Console.WriteLine("Options available for you to choose:");
-            Console.WriteLine("\n 1. String Manipulation \n 2. Palindrome \n 3. Fibonacci \n 4. Array Manipulation");
+            Console.WriteLine("\n 1. String Manipulation \n 2. Palindrome \n 3. Fibonacci \n 4. Prime \n 5. Array Manipulation");
             Console.WriteLine("Enter your option choice:");
             var op = int.TryParse(Console.ReadLine(), out int num);
             if (op)
@@ -173,6 +261,12 @@ class Program()
                         else Console.WriteLine("Please enter a correct positive integer > 0");
                         break;
                     case 4:
+                        Console.WriteLine("Enter a positive integer:");
+                        _ = int.TryParse(Console.ReadLine(), out int inpNum);
+                        if(inpNum > 0) CodingPractice.PrimeCheck(inpNum);
+                        else Console.WriteLine("Please enter a correct positive integer > 0");
+                        break;
+                    case 5:
                         Console.WriteLine("Enter your array (DELIMITER : SPACE) :");
                         Array arr = Console.ReadLine().Split(" ");
                         CodingPractice.ArrManipulation(arr);
